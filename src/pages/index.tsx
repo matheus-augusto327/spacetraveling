ddc264c7e733b1d2c6eb7cf53933bff1206a7245
Matheus <matheus.augustoreal@gmail.com>
2022-05-02 07:13:57 -0300

    Add posts page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,6 @@
 import { GetStaticProps } from 'next';
+import { Head } from 'next/document';
+import { RichText } from 'prismic-dom';
 
 import { getPrismicClient } from '../services/prismic';
 
@@ -24,13 +26,47 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home({ post }: Post) {
+  return (
+    <>
+      <Head>
+        <title>Spacetraveling</title>
+      </Head>
 
-// export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient({});
-//   // const postsResponse = await prismic.getByType(TODO);
+      <main /*className={styles.container}*/>
+        <div /*className={styles.posts}*/>
+          {post}
+        </div>
+      </main>
+    </>
+  )
 
-//   // TODO
-// };
+}
+
+export const getStaticProps = async () => {
+  const prismic = getPrismicClient({});
+  const postsResponse = await prismic.getByType('post', {
+    fetch: ['post.title', 'post.content'],
+    pageSize: 5,
+  });
+
+  const posts = postsResponse.results.map(post => {
+    return {
+      slug: post.uid,
+      title: RichText.asText(post.data.title),
+      excerpt: post.data.content.find(content => content.type === 'paragraph')?.text ?? '',
+      updatedAt: new Date(post.last_publication_date).toLocaleDateString('pt-BR', {
+        day: '2-digit',
+        month: 'long',
+        year: 'numeric'
+      }),
+    }
+  })
+
+  return {
+    props: {
+      posts
+    }
+  }
+
+};
